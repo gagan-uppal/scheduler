3a61fb4bb598672c8b2e0e1e3e72f0cebc232263
Gagandeep Uppal <guppal.profile@gmail.com>
2021-10-28 09:51:53 -0400

    updated with appointment view in storybook

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,15 +1,23 @@
 import React from "react";
-import "components/Appointment/styles.scss";
-import "components/Appointment/styles.scss";
+//import "components/Appointment/styles.scss";
+// import "components/Appointment/styles.scss";
+// import Header from "components/Appointment/Header";
+// import Show from "components/Appointment/Show";
+// import Empty from "components/Appointment/Empty";
 
 
 export default function Appointment(props) {
+  //const {time} = props;
 
   
 
   return (
 
-<article className="appointment">Hello world</article>
+<article className="appointment">
+
+  {props.time ? <span>Appointment at {props.time}</span> : <span>No Appointments</span>}
+  </article>
+ 
 
 
   )
