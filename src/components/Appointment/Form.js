f958d969e8a1c35909cd3762e5f24194a9fa6412
Gagandeep Uppal <guppal.profile@gmail.com>
2021-10-28 13:33:43 -0400

    component Form added with onClick

---
+++ src/components/Appointment/Form.js
@@ -0,0 +1,54 @@
+import React, {useState} from "react";
+import Button from "components/Button";
+import InterviewerList from "components/InterviewerList";
+
+
+export default function Form(props){
+
+const [student, setStudent] = useState(props.name || "");
+const [interviewer, setInterviewer] = useState(props.interviewer || null);
+
+function reset(){
+  setStudent("");
+  setInterviewer(null);
+}
+
+function cancel(){
+  reset();
+  props.onCancel();
+}
+
+
+  return (
+    <main className="appointment__card appointment__card--create">
+  <section className="appointment__card-left">
+    <form autoComplete="off" onSubmit={event => event.preventDefault()}>
+      <input
+        className="appointment__create-input text--semi-bold"
+        name="name"
+        type="text"
+        value={student}
+        placeholder="Enter Student Name"
+        onChange={(event) => setStudent(event.target.value)}
+        /*
+          This must be a controlled component
+          your code goes here
+        */
+      />
+    </form>
+    <InterviewerList 
+      interviewers={props.interviewers}
+      value={interviewer}
+      onChange={setInterviewer}
+      /* your code goes here */
+    />
+  </section>
+  <section className="appointment__card-right">
+    <section className="appointment__actions">
+      <Button danger onClick={() => cancel()}>Cancel</Button>
+      <Button confirm onClick={() => cancel}>Save</Button>
+    </section>
+  </section>
+</main>
+  )
+}
