962d3d66f8f1312f9ceac2d68c229046f08caf78
Gagandeep Uppal <guppal.profile@gmail.com>
2021-10-30 18:36:42 -0400

    adding helper functions

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -12,3 +12,33 @@ export function getAppointmentsForDay(state, day) {
   }
   return result;
 }
+
+export function getInterview(state, interview) {
+
+const obj = {};
+if (!interview || !state) {
+  return null;
+}
+obj["student"] = interview.student;
+obj["interviewer"] = state.interviewers[interview.interviewer];
+
+return obj;
+
+}
+
+export function getInterviewersForDay(state, day) {
+  if(!day) {
+    return [];
+  }
+  const getInterviewer = state.days.filter((x) => x.name === day)[0];
+  if (!getInterviewer) {
+    return []
+  }
+  const interviewers = getInterviewer.interviewers;
+
+  const result = []
+  for(const interviewer of interviewers) {
+    result.push(state.interviewers[interviewer])
+  }
+  return result;
+}
