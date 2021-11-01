bcc05aa16cec0d7c4fcb5acae4f3430e42d231f1
Gagandeep Uppal <guppal.profile@gmail.com>
2021-11-01 15:28:22 -0400

    updated component with getInterviewersForDay

--- src/components/Application.js
+++ src/components/Application.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 //import { useEffect } from "react";
 import axios from "axios";
 //import axios from 'axios';
- //import { getAppointmentsForDay } from "helpers/selectors";
+ import { getAppointmentsForDay, getInterview, getInterviewersForDay } from "helpers/selectors";
 
 import "components/Application.scss";
 import DayList from "./DayList";
@@ -29,44 +29,44 @@ import Appointment from "./Appointment";
 //   },
 // ];
 
-const appointments = [
-  {
-    id: 1,
-    time: "12pm",
-  },
-  {
-    id: 2,
-    time: "1pm",
-    interview: {
-      student: "Lydia Miller-Jones",
-      interviewer:{
-        id: 3,
-        name: "Sylvia Palmer",
-        avatar: "https://i.imgur.com/LpaY82x.png",
-      }
-    }
-  },
-  {
-    id: 3,
-    time: "2pm",
-  },
-  {
-    id: 4,
-    time: "3pm",
-    interview: {
-      student: "Archie Andrews",
-      interviewer:{
-        id: 4,
-        name: "Cohana Roy",
-        avatar: "https://i.imgur.com/FK8V841.jpg",
-      }
-    }
-  },
-  {
-    id: 5,
-    time: "4pm",
-  }
-];
+// const appointments = [
+//   {
+//     id: 1,
+//     time: "12pm",
+//   },
+//   {
+//     id: 2,
+//     time: "1pm",
+//     interview: {
+//       student: "Lydia Miller-Jones",
+//       interviewer:{
+//         id: 3,
+//         name: "Sylvia Palmer",
+//         avatar: "https://i.imgur.com/LpaY82x.png",
+//       }
+//     }
+//   },
+//   {
+//     id: 3,
+//     time: "2pm",
+//   },
+//   {
+//     id: 4,
+//     time: "3pm",
+//     interview: {
+//       student: "Archie Andrews",
+//       interviewer:{
+//         id: 4,
+//         name: "Cohana Roy",
+//         avatar: "https://i.imgur.com/FK8V841.jpg",
+//       }
+//     }
+//   },
+//   {
+//     id: 5,
+//     time: "4pm",
+//   }
+// ];
 
 export default function Application(props) {
   // const [day, setDay] = useState("Monday");
@@ -77,19 +77,55 @@ export default function Application(props) {
     day: "Monday",
     days: [],
     // you may put the line below, but will have to remove/comment hardcoded appointments variable
-    appointments: {}
+    appointments: {},
+    interviewers: {}
+
   });
+
+  //const dailyAppointments = [];
+  //const dailyAppointments = getAppointmentsForDay//(state, state.day);
+
+
   const setDay = day => setState({ ...state, day });
-  const setDays = days => setState(prev => ({ ...prev, days }));
+  //const setDays = days => setState(prev => ({ ...prev, days }));
 
   useEffect(() => {
-    axios.get('/api/days')
-      .then((res) => {
-        setDays(res.data)
-      })
+  //   axios.get('/api/days')
+  //     .then((res) => {
+  //       setDays(res.data)
+  //     })
 
-  }, [])
+  // }, [])
+  Promise.all([
+    axios.get('/api/days'),
+    axios.get('/api/appointments'),
+    axios.get('/api/interviewers'),
+  ])
+    .then((all) => {
+      const days = all[0].data;
+      const appointments = all[1].data;
+      const interviewers = all[2].data;
+      setState(prev => ({ ...prev, days, appointments, interviewers })) // saves new state and replaces writing state.days and state.appointments
+    })
+}, [])
 
+const dailyAppointments = getAppointmentsForDay(state, state.day);
+const dailyInterviewers = getInterviewersForDay(state, state.day);
+// const appointments = getAppointmentsForDay(state, state.day);
+
+  const schedule = dailyAppointments.map(appointment => {
+
+    const interview = getInterview(state, appointment.interview);
+    return (
+    <Appointment
+    key={appointment.id}
+    id={appointment.id}
+    time={appointment.time}
+    interview={interview}
+    interviewers={dailyInterviewers}
+    />
+  );
+    })
   return (
     <main className="layout">
       <section className="sidebar">
@@ -115,15 +151,11 @@ export default function Application(props) {
       </section>
       <section className="schedule">
         {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
-        {appointments.map(appointment => {
-          return <Appointment
-          key={appointment.id}
-          id={appointment.id}
-          time={appointment.time}
-          interview={appointment.interview}
-          />
-        })
-      }
+        {/* {dailyAppointments.map(appointment => {
+                      const interview = getInterview(state, appointment.interview); */}
+
+      {schedule}
+
        <Appointment key="last" time="5pm" />
 
         
