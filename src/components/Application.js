06a5dcd893f7f266de03845ff16eac8ae3f2ee4c
Gagandeep Uppal <guppal.profile@gmail.com>
2021-11-03 15:45:36 -0400

    spacing and formatting done

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,166 +1,22 @@
 import React from "react";
-//import { useEffect } from "react";
-//import axios from "axios";
-//import axios from 'axios';
 import { useApplicationData } from "hooks/useApplicationData";
-
- import { getAppointmentsForDay, getInterview, getInterviewersForDay } from "helpers/selectors";
-
+import { getAppointmentsForDay, getInterview, getInterviewersForDay } from "helpers/selectors";
 import "components/Application.scss";
 import DayList from "./DayList";
 import Appointment from "./Appointment";
 
-
-//const [day, setDay] = useState("Monday");
-
-// const days = [
-//   {
-//     id: 1,
-//     name: "Monday",
-//     spots: 2,
-//   },
-//   {
-//     id: 2,
-//     name: "Tuesday",
-//     spots: 5,
-//   },
-//   {
-//     id: 3,
-//     name: "Wednesday",
-//     spots: 0,
-//   },
-// ];
-
-// const appointments = [
-//   {
-//     id: 1,
-//     time: "12pm",
-//   },
-//   {
-//     id: 2,
-//     time: "1pm",
-//     interview: {
-//       student: "Lydia Miller-Jones",
-//       interviewer:{
-//         id: 3,
-//         name: "Sylvia Palmer",
-//         avatar: "https://i.imgur.com/LpaY82x.png",
-//       }
-//     }
-//   },
-//   {
-//     id: 3,
-//     time: "2pm",
-//   },
-//   {
-//     id: 4,
-//     time: "3pm",
-//     interview: {
-//       student: "Archie Andrews",
-//       interviewer:{
-//         id: 4,
-//         name: "Cohana Roy",
-//         avatar: "https://i.imgur.com/FK8V841.jpg",
-//       }
-//     }
-//   },
-//   {
-//     id: 5,
-//     time: "4pm",
-//   }
-// ];
-
 export default function Application(props) {
-  // const [day, setDay] = useState("Monday");
-  // const [days, setDays] = useState([]);
-  // const [appointments, setAppointments] = useState({})
   
-  // const [state, setState] = useState({
-  //   day: "Monday",
-  //   days: [],
-  //   // you may put the line below, but will have to remove/comment hardcoded appointments variable
-  //   appointments: {},
-  //   interviewers: {}
-
-  // });
-
-  //const dailyAppointments = [];
-  //const dailyAppointments = getAppointmentsForDay//(state, state.day);
-
-  // function bookInterview(id, interview) {
-  //   const appointment = {
-  //     ...state.appointments[id],
-  //     interview: { ...interview }
-  //   };
-  
-  //   const appointments = {
-  //     ...state.appointments,
-  //     [id]: appointment
-  //   };
-  
-  //   return axios.put(`/api/appointments/${id}`, { interview })
-  //     .then(() => {
-  //       setState({ ...state, appointments })
-  //     })
-  // };
-
-  // function cancelInterview(id) {
-  //   const appointment = {
-  //     ...state.appointments[id],
-  //     interview: null,
-  //   };
-  //   const appointments = {
-  //     ...state.appointments,
-  //     [id]: appointment
-  //   };
-  //   return axios.delete(`/api/appointments/${id}`)
-  //     .then(() => {
-  //       setState({ ...state, appointments })
-  //     })
-  // }
-
-  // const setDay = day => setState({ ...state, day });
-  // //const setDays = days => setState(prev => ({ ...prev, days }));
-
-//   useEffect(() => {
-//   //   axios.get('/api/days')
-//   //     .then((res) => {
-//   //       setDays(res.data)
-//   //     })
-
-//   // }, [])
-//   Promise.all([
-//     axios.get('/api/days'),
-//     axios.get('/api/appointments'),
-//     axios.get('/api/interviewers'),
-//   ])
-//     .then((all) => {
-//       const days = all[0].data;
-//       const appointments = all[1].data;
-//       const interviewers = all[2].data;
-//       setState(prev => ({ ...prev, days, appointments, interviewers })) // saves new state and replaces writing state.days and state.appointments
-//     })
-// }, [])
-const {
-  state,
-  setDay,
-  bookInterview,
-  cancelInterview
-} = useApplicationData();
-
+const { state, setDay, bookInterview, cancelInterview } = useApplicationData();
 
 const dailyAppointments = getAppointmentsForDay(state, state.day);
 const dailyInterviewers = getInterviewersForDay(state, state.day);
-// const appointments = getAppointmentsForDay(state, state.day);
-
 
   const schedule = dailyAppointments.map(appointment => {
-
     const interview = getInterview(state, appointment.interview);
     return (
     <Appointment
     key={appointment.id} {...appointment}
-    // id={appointment.id}
     time={appointment.time}
     interview={interview}
     interviewers={dailyInterviewers}
@@ -168,41 +24,33 @@ const dailyInterviewers = getInterviewersForDay(state, state.day);
     cancelInterview={cancelInterview}
     />
   );
-    })
+})
   return (
     <main className="layout">
       <section className="sidebar">
-        {/* Replace this with the sidebar elements during the "Project Setup & Familiarity" activity. */}
         <img
-  className="sidebar--centered"
-  src="images/logo.png"
-  alt="Interview Scheduler"
-/>
-<hr className="sidebar__separator sidebar--centered" />
-<nav className="sidebar__menu">
-<DayList
-  days={state.days}
-  day={state.day}
-  setDay={setDay}
-/>
-</nav>
-<img
-  className="sidebar__lhl sidebar--centered"
-  src="images/lhl.png"
-  alt="Lighthouse Labs"
-/>
-      </section>
+          className="sidebar--centered"
+          src="images/logo.png"
+          alt="Interview Scheduler"
+        />
+        <hr className="sidebar__separator sidebar--centered" />
+          <nav className="sidebar__menu">
+            <DayList
+              days={state.days}
+              day={state.day}
+              setDay={setDay}
+            />
+          </nav>
+        <img
+          className="sidebar__lhl sidebar--centered"
+          src="images/lhl.png"
+          alt="Lighthouse Labs"
+        />
+    </section>
       <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
-        {/* {dailyAppointments.map(appointment => {
-                      const interview = getInterview(state, appointment.interview); */}
-
       {schedule}
-
-       <Appointment key="last" time="5pm" />
-
-        
-      </section>
+        <Appointment key="last" time="5pm" />
+     </section>
     </main>
   );
 }
